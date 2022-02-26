c0fff802280fd2600d34c75e1b579912f59f7c20
niuxiangchen <1415055998@qq.com>
2022-02-26 15:10:11 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,40 @@
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+const Container = styled.div`
+  //display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main``;
