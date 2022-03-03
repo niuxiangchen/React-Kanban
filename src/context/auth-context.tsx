e5b697dcb9c9538c02bb402ed4ab80a38091f4c3
niuxiangchen <1415055998@qq.com>
2022-03-04 00:26:14 +0800

    feat: 处理me接口 loading和error

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,8 @@ import * as auth from "../auth-provider";
 import { User } from "../screens/project-list/search-panel";
 import { http } from "../utils/http";
 import { useMount } from "../utils";
+import { useAsync } from "../utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "../components/lib";
 
 interface AuthForm {
   username: string;
@@ -32,8 +34,15 @@ const AuthContext = React.createContext<
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
-
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   // const login = (form: AuthForm) =>
   //   auth.login(form).then((user) => setUser(user));
   //语法糖
@@ -43,8 +52,17 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   //页面加载时调用
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+
+  //当初始时或加载时
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+  //当查询出错的时候
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   return (
     <AuthContext.Provider
