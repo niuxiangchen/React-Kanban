55f70d15653d813cfb5ddb1bb6da53421a666e03
niuxiangchen <1415055998@qq.com>
2022-03-09 08:55:18 +0800

    feat: 增加errorBoundary

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -7,6 +7,7 @@ import { Button, Dropdown, Menu } from "antd";
 
 export const AuthenticatedApp = () => {
   const { logout, user } = useAuth();
+  const value: any = undefined;
   return (
     <Container>
       <Header between={true}>
